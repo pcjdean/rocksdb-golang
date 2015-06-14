4b97aa76ce52f48c294ee6dfb766b75a68b1a0bd
deanpan <dean_pan@hotmail.com>
2015-06-14 23:53:01 +1000

    add db.go

---
+++ db.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, Dean ChaoJun Pan.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+package rocksdb
+
+/*
+#cgo LDFLAGS: -lrocksdb -lstdc++ -lz -lrt
+#include "db.h"
+*/
+import "C"
